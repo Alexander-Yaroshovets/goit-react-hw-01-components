23ecbd1bb2eaed1f48affa00336aa315e9310837
Alexander <Alexyaroshovets300687@gmail.com>
2022-11-15 10:11:36 +0200

    firstcommit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Profile } from './components/Profile';
-import { App } from 'components/App';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
